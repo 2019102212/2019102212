18c7eea6df0857d0a5f78ddb30ad61cd0c8838c9
김대휘 <dhxl50@khu.ac.kr>
2020-04-17 13:53:16 +0900

    9 times table

--- 2019102212.cpp
+++ 2019102212.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-	for (int i = 1; i < 9; i++) {
+	for (int i = 1; i < 10; i++) {
 		cout << i << "´Ü" << endl;
-		for (int j = 1; j < 9; j++) {
+		for (int j = 1; j < 10; j++) {
 			cout << i << " * " << j << " = " << i * j << endl;
  		}
 		cout << endl;
